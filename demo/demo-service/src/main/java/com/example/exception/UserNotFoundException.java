a87e7416d907efc56a7f3e51f334d8f0f588ba3c
yygs321 <yygs321@naver.com>
2025-08-06 11:20:34 +0900

    Remove: 기존 demo 모듈 전체 삭제

--- demo/demo-service/src/main/java/com/example/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id) {
-        super("User not found with id: " + id);
-    }
-}
