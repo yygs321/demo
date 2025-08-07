1b74022e75854903412f8122e621240dea81a3cb
yygs321 <yygs321@naver.com>
2025-08-07 10:52:19 +0900

    refactor/ User에서 Employee로 명칭 변경

---
+++ demo-domain/src/main/java/com/example/spec/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.spec;
+
+import com.example.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee getEmployeeById(Long id);
+    List<Employee> getAllEmployees();
+    void createEmployee(Employee employee);
+    void updateEmployee(Long id, Employee employee);
+    void deleteEmployee(Long id);
+    void checkEmployeeExists(Long id);
+}
