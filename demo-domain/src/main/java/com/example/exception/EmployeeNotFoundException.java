d3042d283fa0ae765d2a41e525602996544f679f
yygs321 <yygs321@naver.com>
2025-08-08 16:42:11 +0900

    refactor: 예외처리 구조 변경으로 기존 예외 및 응답 관련 클래스 삭제

--- demo-domain/src/main/java/com/example/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exception;
-
-public class EmployeeNotFoundException extends BusinessException {
-    public EmployeeNotFoundException(Long id) {
-        super("Employee not found with id: " + id);
-    }
-}
